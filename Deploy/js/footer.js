e23aff90a09ba0f20c3b6d8ad770e2bcb2936aaa
danielrasmuson <dan123911@gmail.com>
2013-11-30 18:32:30 -0600

    Initial Push
    Setup a basic website, choose a theme, customized it then added changes
    on pages.

---
+++ Deploy/js/footer.js
@@ -0,0 +1,2 @@
+var footer = "</div><div id=\"footer\">    <p class=\"legal\">Copyright (c) 2007 Ultimate Impact. All rights reserved.</p>    <div id=\"facebookIcon\">        <a href=\"https://www.facebook.com/events/377075219085154/\">            <img src=\"../images/fbicon.png\" alt=\"\">        </a>    </div></div></div></body></html>"
+document.write(footer)
